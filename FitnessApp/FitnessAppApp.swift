240eb6b181a583d2c077349724f5502aa78b85c8
Mark Henry <marknhenry@live.com>
2025-01-23 21:11:53 +0400

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Mark Henry on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
