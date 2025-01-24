1ba0c81a7c1b7b98126cfed1f7b65e3f9d99dc5b
Mark Henry <marknhenry@live.com>
2025-01-25 02:13:24 +0400

    ActivityCard func

--- FitnessApp/Home/HomeView.swift
+++ FitnessApp/Home/HomeView.swift
@@ -12,9 +12,16 @@ struct HomeView: View {
     @State var active: Int = 52
     @State var stand: Int = 8
     
+    var mockActivities = [
+        Activity(id: 0, title: "Today Steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .green, amount: "6,234"),
+        Activity(id: 1, title: "Today Steps", subtitle: "Goal 1,000", image: "figure.walk", tintColor: .red, amount: "812"),
+        Activity(id: 2, title: "Today Steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .blue, amount: "6,234"),
+        Activity(id: 3, title: "Today Steps", subtitle: "Goal 60,000", image: "figure.walk", tintColor: .purple, amount: "6,234")
+    ]
+    
     var body: some View {
         ScrollView(showsIndicators:false) {
-            VStack {
+            VStack (alignment: .leading) {
                 Text("Welcome")
                     .font(.largeTitle)
                     .padding()
@@ -62,6 +69,28 @@ struct HomeView: View {
                     Spacer()
                 }
                 .padding()
+                HStack{
+                    Text("Fitness Activity")
+                        .font(.title2)
+                    Spacer()
+                    
+                    Button{
+                        print("Show more")
+                    } label: {
+                        Text("Show more")
+                            .padding(.all, 10)
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(20)
+                    }
+                }
+                .padding(.horizontal)
+                LazyVGrid(columns: Array(repeating: GridItem(spacing:20), count: 2)){
+                    ForEach(mockActivities, id: \.id) { activity in
+                        ActivityCard(activity: activity)
+                    }
+                }
+                .padding(.horizontal)
             }
         }
     }
